e282725d6cf56810c61c0d008f5b62bb2baa72c3
VIKTOR CHIARCOS <viktor.chiarcos@gmail.com>
2026-08-24 14:31:52 +0200

    Refactor custom elements and update selectors

--- cssml.js
+++ cssml.js
@@ -1,36 +1,34 @@
+//NEU
 // 1. Das Element einmalig beim Browser registrieren
-    class UnderlineElement extends HTMLElement {
-        constructor() {
-            super();
-        }
+class UnderlineElement extends HTMLElement {
+    constructor() {
+        super();
     }
-    customElements.define('u', UnderlineElement);
+}
+customElements.define('u-element', UnderlineElement);
 
-    // 2. ALLE <u> Elemente auf der Seite finden
-    const All_u_Elements = document.querySelectorAll('u');
+// 2. ALLE u-element Elemente auf der Seite finden
+const allUElements = document.querySelectorAll('u-element');
 
-    // 3. Jedes einzelne Element durchgehen und die Attribute auslesen
-    All_u_Elements.forEach((content, index) => {
+// 3. Jedes einzelne Element durchgehen und den Stil anwenden
+allUElements.forEach((content) => {
+    const old = content.innerHTML;
+    content.innerHTML = `<div style="text-decoration: underline">${old}</div>`;
+});
 
-        const old = content.innerHTML;
-        content.innerHTML=`<div style="text-decoration: underline">${old}</div>`
-    });
-
-
-// 1. Das Element einmalig beim Browser registrieren
-    class OverlineElement extends HTMLElement {
-        constructor() {
-            super();
-        }
+// 1. Das Overline-Element einmalig beim Browser registrieren
+class OverlineElement extends HTMLElement {
+    constructor() {
+        super();
     }
-    customElements.define('o', OverlineElement);
+}
+customElements.define('o-element', OverlineElement);
 
-    // 2. ALLE <u> Elemente auf der Seite finden
-    const All_o_Elements = document.querySelectorAll('o');
+// 2. ALLE o-element Elemente auf der Seite finden
+const allOElements = document.querySelectorAll('o-element');
 
-    // 3. Jedes einzelne Element durchgehen und die Attribute auslesen
-    All_o_Elements.forEach((o_content, index) => {
-
-        const old = o_content.innerHTML;
-        o_content.innerHTML=`<div style="text-decoration: overline">${old}</div>`
-    });
+// 3. Jedes einzelne Element durchgehen und den Stil anwenden
+allOElements.forEach((o_content) => {
+    const old = o_content.innerHTML;
+    o_content.innerHTML = `<div style="text-decoration: overline">${old}</div>`;
+});
